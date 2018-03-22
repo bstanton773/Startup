479b3dd8dd2f9492a7ff478e85d88f826a4315f8
Unknown <bstanton773@gmail.com>
2018-03-22 13:47:12 -0500

    working chart

--- hello.py
+++ hello.py
@@ -3,11 +3,9 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 @app.route('/index')
 def weather():
-    return "hello World!"
-    render_template('index.html')
+    return render_template('index.html')
 
 app.run()
-
